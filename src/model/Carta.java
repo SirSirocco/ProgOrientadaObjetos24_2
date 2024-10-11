8ce1f58c43c93095b408ed670c2cd3455d4d3609
João Marcello <marcello.jao1611@gmail.com>
2024-10-11 18:35:04 -0300

    classe FonteCarta completo, classe Carta parcialmente completo

---
+++ src/model/Carta.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Carta {
+	String naipe;
+	String valor;
+	
+	public Carta(String naipe, String valor) {
+		this.naipe = naipe;
+		this.valor = valor;
+	}
+
+	void GetNaipe() {
+	}
+	
+	void GetValor() {
+	}
+}
---
+++ src/model/FonteCarta.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.util.*;
+
+public class FonteCarta {
+	final int numBaralhos = 8;
+	int contCartas;
+	private List<Carta> fonte, copiaOrdenada;
+	
+	public FonteCarta() {
+		fonte = new ArrayList<>();
+		copiaOrdenada = new ArrayList<>();
+		String[] naipes = {"Paus", "Ouros", "Espadas", "Copas"};
+		String[] valores = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}; 
+		
+		for (String naipe: naipes) {
+			for (String valor: valores) {
+				fonte.add(new Carta(naipe, valor));
+				copiaOrdenada.add(new Carta(naipe, valor));
+			}
+		}
+		
+	}
+	
+	boolean checaEmbaralha() {
+		return fonte.equals(copiaOrdenada);
+	}
+	
+	void embaralha() {
+		Collections.shuffle(fonte);
+	}
+	
+	public Carta compraCarta() {
+		return fonte.remove(0);
+	}
+
+}
