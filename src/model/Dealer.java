b7dea35878b55f4890a54b8d37a24d5c35da31a1
Pedro Barizon <pedro.a.barizon@gmail.com>
2024-10-13 22:38:05 -0600

    v_1.2.5 - Merge resolvido (eu acho)

--- src/model/Dealer.java
+++ src/model/Dealer.java
@@ -8,14 +8,3 @@ class Dealer extends Participante {
 		Participante.dealer = this; // Preenche campo estatico de Participante com objeto dealer criado
 	}
 }
-
-package model;
-
-class Dealer extends Participante {
-	private static final int maosMaxDealer = 1; // Numero maximo de maos do dealer
-	
-	Dealer() {
-		super(maosMaxDealer);
-		Participante.dealer = this; // Preenche campo estatico de Participante com objeto dealer criado
-	}
-}
