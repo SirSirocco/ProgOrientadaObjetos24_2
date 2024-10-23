95402bc56c3cd817fcf55a8ccee8a7709b471b0e
João Marcello <marcello.jao1611@gmail.com>
2024-10-23 18:37:05 -0300

    melhor modularização e devido posicionamento relativo ao tamanho da tela

--- src/view/Banca.java
+++ src/view/Banca.java
@@ -1,21 +1,42 @@
 package view;
 
 import java.awt.*;
+import java.io.*;
 import javax.swing.*;
+import javax.imageio.ImageIO;
 
 public class Banca extends JFrame{
+	int x, y, width, heigth;
 	JPanel p;
+	Image i;
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	
-	public Banca(String s, Image image, int x, int y) {
-		super(s);
+	public Banca() {
+		// Define o nome da janela
+		super("Banca");
 		setLayout(null);
+
+		// Pega a imagem de fundo
+		try {
+			i = ImageIO.read(new File("Imagens/blackjack.png"));
+		}
+		catch (IOException e) {
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+
+		// Define o tamanho e posição da janela
+		width = i.getWidth(null) + 15;
+		heigth = i.getHeight(null) + 39;
+		x = ((int)screenSize.getWidth() - width)/2;
+		//y = ((int)screenSize.getHeight() - heigth)/2;
 		
-		p = new BPanel(image);
+		p = new BPanel(i);
 		p.setBackground(Color.WHITE);
-		p.setBounds(0, 0, x, y);
+		p.setBounds(0, 0, width, heigth);
 		getContentPane().add(p);
-		
-		setSize(x, y);
+
+		setBounds(x, 0, width, heigth);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,30 +1,8 @@
 package view;
 
-import java.awt.Image;
-import java.io.*;
-import javax.imageio.ImageIO;
-
 public class Main {
 	public static void main(String[] args) {
-		Banca b;
-		Image i = null;
-		int x, y;
-		
-		try {
-			i = ImageIO.read(new File("Imagens/blackjack.png"));
-		}
-		catch (IOException e) {
-			System.out.println(e.getMessage());
-			System.exit(1);
-		}
-		
-		x = i.getWidth(null) + 15;
-		y = i.getHeight(null) + 39;
-		
-		System.out.println(x);
-		System.out.println(y);
-		
-		b = new Banca ("banca", i, x, y);
+		Banca b = new Banca();
 		b.setVisible(true);
 
 	}
