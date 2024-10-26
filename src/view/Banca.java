b34f9555d9ecc02d0d7dbbb083ffb3d6ea761f03
João Marcello <marcello.jao1611@gmail.com>
2024-10-26 16:59:39 -0300

    janela mostra o deck de cartas na mesa + função para pegar as imagens das cartas

--- src/view/Banca.java
+++ src/view/Banca.java
@@ -8,7 +8,7 @@ import javax.imageio.ImageIO;
 class Banca extends JFrame{
 	int x, y, width, heigth;
 	JPanel p;
-	Image backgroundImage, fichas[] = new Image[6];
+	Image deck, backgroundImage, fichas[] = new Image[6];
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	
 	public Banca() {
@@ -27,7 +27,8 @@ class Banca extends JFrame{
 		fichas[3] = pegaImagem("Imagens/ficha 20$.png");
 		fichas[4] = pegaImagem("Imagens/ficha 50$.png");
 		fichas[5] = pegaImagem("Imagens/ficha 100$.png");
-
+		
+		deck = pegaImagem("Imagens/deck1.gif");
 
 		// Define o tamanho e posição da janela
 		width = backgroundImage.getWidth(null) + 15;
@@ -39,14 +40,14 @@ class Banca extends JFrame{
 		saveButton.setBounds(15, 530, 170, 40);
 
 		// Define o painel da imagem de fundo
-		p = new BPanel(backgroundImage, fichas);
+		p = new BPanel(backgroundImage, fichas, deck);
 		p.setBackground(Color.WHITE);
 		p.setBounds(0, 0, width, heigth);
 		
 		// Adiciona os componentes ao painel
 		c.add(saveButton);
 		c.add(p);
-
+		
 		setBounds(x, 0, width, heigth);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
@@ -58,6 +59,49 @@ class Banca extends JFrame{
 		}
 		catch (IOException e) {
 			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * Função para pegar a imagem da carta de acordo com os naipes e valores
+	 * @param naipe
+	 * @param valor
+	 * @return imagem da carta
+	 */
+	Image pegaCarta(String naipe, String valor) {
+		try {
+			Integer.parseInt(valor);
+		} catch (NumberFormatException e) {
+			valor = valor.toLowerCase();
+		}
+
+		switch (naipe) {
+		case "Paus":
+			naipe = "c";
+			break;
+		case "Ouros":
+			naipe = "d";
+			break;
+		case "Espadas":
+			naipe = "s";
+			break;
+		case "Copas":
+			naipe = "h";
+			break;
+		default:
+			System.out.println("naipe inválido");
+			break;
+		}
+		
+		String caminho = "Imagens/" + valor + naipe + ".gif";
+		Image result = null;
+		try {
+			result = ImageIO.read(new File(caminho));
+		} catch (IOException e) {
+			System.out.println(e);
 			System.exit(1);
 		}
 		
